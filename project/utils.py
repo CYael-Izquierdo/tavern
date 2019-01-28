1aca427e0572d1fe23c77ff0a8e21a00d0d6f74a
cizquierdo <cizquierdo@belatrixsf.com>
2019-01-28 10:45:43 -0300

    Get all project assertions fixed

--- project/utils.py
+++ project/utils.py
@@ -7,12 +7,13 @@ def check_projects(response):
     with soft_assertions():
         assert_that(len(projects)).is_less_than_or_equal_to(25)
         for project in projects:
-            assert_that(project.get('id')).is_type_of(int)
-            # assert_that(project.get('name')).is_instance_of(str)
-            # assert_that(project.get('identifier')).is_instance_of(str)
-            # assert_that(project.get('description')).is_instance_of(str)
-            assert_that(project.get('status')).is_type_of(int)
-            assert_that(project.get('is_public')).is_type_of(bool)
+            assert_that(project.get('id')).described_as('id').is_not_none().is_type_of(int)
+            assert_that(project.get('name')).described_as('name').is_not_none().is_type_of(str)
+            assert_that(project.get('identifier')).described_as('identifier').is_not_none().is_type_of(str)
+            if project.get('description', False):
+                assert_that(project.get('description')).described_as('description').is_not_none().is_type_of(str)
+            assert_that(project.get('status')).described_as('status').is_not_none().is_type_of(int)
+            assert_that(project.get('is_public')).described_as('is_public').is_not_none().is_type_of(bool)
 
 
 
